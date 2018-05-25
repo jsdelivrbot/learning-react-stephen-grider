a68f57175a5acb770f3ebda3794807be1073c43c
Waldo Lavaut <w86compositor@gmail.com>
2018-05-25 10:03:06 -0400

    cleanup a bit the project from non useful folders

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-    render () {
-      return <input onChange={ event => { console.log(event.target.value); } } />;
-    }
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = { term: 'Starting value' };
+  }
+  render() {
+    return (
+      <div>
+        <input 
+        value={this.state.term}
+        onChange={event => this.setState({ term: event.target.value })} />
+      </div>
+    );
+  }
+};
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
-const API_KEY = '';
+const API_KEY = 'AIzaSyANSfLh_-TgYGov1XexRBIR3XxAsOwEMAk';
 
-const App = () => {
-  render(
-    <div>
-      <SearchBar />
-    </div>
-  )
+//Create a new component. This component should produce some HTML
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({ videos }); // This only works when the key and value are the exact same "word" but React will translate this to the line below
+      //this.setState( { videos: videos} )
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />>
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<SearchBar />, document.querySelector('.container'));
